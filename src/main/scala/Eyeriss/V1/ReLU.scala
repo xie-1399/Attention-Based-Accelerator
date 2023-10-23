6c319889c6c7a40c794ced6be28ca3ab99ef9f38
xxl <3161932956@qq.com>
2023-10-23 23:24:40 +0800

    finish Relu logic

--- src/main/scala/Eyeriss/V1/ReLU.scala
+++ src/main/scala/Eyeriss/V1/ReLU.scala
@@ -1,5 +1,6 @@
 package Eyeriss.V1
 
+import DefineMem.FIFO
 import DefineSim.SpinalSim.{PrefixComponent, RtlConfig}
 import spinal.core._
 import spinal.lib._
@@ -10,7 +11,8 @@ import spinal.lib._
 case class ActParameters(
                         dataWidth:Int = 16,
                         dataLen:Int = 32,
-                        pipe:Boolean = true /*  build the pipeline */
+                        pipe:Boolean = true, /*  build the pipeline */
+                        fifoDepth:Int = 16
                         ){
   def bitsNum  = dataLen * dataWidth
 }
@@ -23,22 +25,54 @@ class ReLU(p:ActParameters) extends PrefixComponent{
   val io = new Bundle{
     val dataIn = slave Stream(Vec(SInt(dataWidth bits),dataLen))
     val dataOut = master Stream(Vec(SInt(dataWidth bits),dataLen))
+    val block = ifGen(pipe) { out Bool() }
   }
-  val busy = RegInit(False)
-  io.dataIn.ready := !busy
 
   /* one cycle get value */
   val logic = ifGen(!pipe){
+    val busy = RegInit(False)
+    io.dataIn.ready := !busy
     busy := False
     val sequence = io.dataIn.payload.map(x => Mux(x > 0, x , zero))
     io.dataOut.payload.assignFromBits(sequence.asBits())
     io.dataOut.valid := io.dataIn.valid
   }
 
-  val pipeline = ifGen(pipe){} // Todo
+
+  /* seem the fifo structure is not ready */
+
+  val pipeline = ifGen(pipe){
+    /* build it with pipeline */
+
+    val fifo = new FIFO(io.dataIn.payloadType,entries = fifoDepth) /* the fifo is used to save the payload */
+    io.dataIn >-> fifo.io.enqueue
+
+    fifo.io.dequeue.ready := True
+    val pass = False
+    io.dataOut.valid := False
+
+    val stage1 = new Area {
+      val load = fifo.io.dequeue.payload
+      val sequence = load.map(x => Mux(x > 0, x , zero))
+      when(fifo.io.dequeue.fire){
+        pass := True
+      }
+    }
+
+    val stage2 = new Area{
+      when(pass){
+        fifo.io.dequeue.ready := False
+        io.dataOut.valid := True
+      }
+      io.dataOut.payload.assignFromBits(stage1.sequence.asBits())
+
+    }
+    io.block := fifo.io.count === fifoDepth - 1
+
+  }
 
 }
 
 object ReLU extends App{
-  val rtl = new RtlConfig().GenRTL(top = new ReLU(ActParameters(pipe = false)))
+  val rtl = new RtlConfig().GenRTL(top = new ReLU(ActParameters(pipe = true)))
 }
