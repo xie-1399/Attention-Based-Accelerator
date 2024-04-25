3016590a1abd862528192023e3726a7268b238be
xie-1399 <2267298047@qq.com>
2024-04-25 11:16:12 +0800

    finish the vit model ...

--- Algorithm/VisionTransformer/VIT/Model.py
+++ Algorithm/VisionTransformer/VIT/Model.py
@@ -1,6 +1,12 @@
 # vision transformer model with only Encoder
 # the source code is from the https://github.com/jeonsworld/ViT-pytorch/blob/main/models/modeling.py
 # difference is without resnet part
+
+# the ops in the VIT model should support like
+# (1) matmul (2)softmax (3) transpose (4) simd include the vec div (5) Layer normal
+# looks like the linear and attention is most popular operations
+
+import copy
 import math
 
 import torch
@@ -9,6 +15,7 @@ import numpy as np
 from torch.nn.modules.utils import _pair
 import config
 # make the img to patches and embedding the patches
+ACT2FN = {"gelu":nn.functional.gelu,"relu":nn.functional.relu}
 
 # let the embeddings on the CPU or GPU to get the value
 class Embeddings(nn.Module):
@@ -92,20 +99,101 @@ class Attention(nn.Module): # get the q k v and attention scores
 class MLP(nn.Module):
     def __init__(self,config):
         super(MLP,self).__init__()
-        self.fc1 = nn.Linear(config.hidden_size,config.transformer["mlp_dim"])
+        self.fc1 = nn.Linear(config.hidden_size,config.transformer["mlp_dim"]) # init is with bias
+        self.fc2 = nn.Linear(config.transformer["mlp_dim"],config.hidden_size)
+        self.act_fn = ACT2FN["gelu"]
+        self.dropout = nn.Dropout(config.transformer["dropout_rate"])
+        self._init_weights()
 
+    def _init_weights(self):
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.normal_(self.fc1.bias,std=1e-6)
+        nn.init.normal_(self.fc2.bias,std=1e-6)
+
+    def forward(self,x): # the mlp net forward pass
+        x = self.fc1(x)
+        x = self.act_fn(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.dropout(x)
+        return x
+
+# transformer block with Layer norm
 class Block(nn.Module):
-    pass
 
+    def __init__(self,config,vis):
+        super(Block,self).__init__()
+        self.hidden_size = config.hidden_size
+        self.attention_norm = nn.LayerNorm(config.hidden_size,eps=1e-6)
+        self.ffn_norm = nn.LayerNorm(config.hidden_size,eps=1e-6)
+        self.ffn = MLP(config)
+        self.attn = Attention(config,vis)
 
+    def forward(self,x):
+        h = x
+        x = self.attention_norm(x)
+        x = self.attn(x)
+        x = x + h
+
+        h = x
+        x = self.ffn_norm(x)
+        x = self.ffn(x)
+        x = x + h
+        return x
+
+# with num_layers block on the encode（12 layers）
 class Encoder(nn.Module):
-    pass
+    def __init__(self,config,vis):
+        super(Encoder,self).__init__()
+        self.vis = vis
+        self.layer = nn.ModuleList()
+        self.encode_norm = nn.LayerNorm(config.hidden_size,eps=1e-6)
+        for _ in range(config.transformer["num_layers"]): # add the layers in the nn module list
+            layer = Block(config, vis)
+            self.layer.append(copy.deepcopy(layer))
 
+    def forward(self,hidden_states):
+        for layer_block in self.layer:
+            hidden_states = layer_block(hidden_states)
+        encoded = self.encode_norm(hidden_states)
+        return encoded
+
+class Transformer(nn.Module):
+    def __init__(self, config, img_size, vis):
+        super(Transformer, self).__init__()
+        self.embeddings = Embeddings(config, img_size=img_size)
+        self.encoder = Encoder(config, vis)
+
+    def forward(self, input_ids):
+        embedding_output = self.embeddings(input_ids)
+        encoded = self.encoder(embedding_output)
+        return encoded
+
+# add the img class
+class VisionTransformer(nn.Module):
+    def __init__(self, config, img_size=224, num_classes=21843, zero_head=False, vis=False):
+        super(VisionTransformer, self).__init__()
+        self.num_classes = num_classes
+        self.zero_head = zero_head
+        self.classifier = config.classifier
+
+        self.transformer = Transformer(config, img_size, vis)
+        self.head = nn.Linear(config.hidden_size, num_classes)
+
+    def forward(self, x, labels=None):
+        x = self.transformer(x)
+        logits = self.head(x[:, 0])
+
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, self.num_classes), labels.view(-1))
+            return loss
+        else:
+            return logits
 
 if __name__ == '__main__':
     config = config.get_b16_config()
-    embeddings = Embeddings(config,224,3)
-    attn = Attention(config,False)
-    x1 = embeddings(torch.randn(1,3,224,224))  # [N C H W] for the Conv2D
-    x2 = attn(x1)
-    print(x2)
+    img = torch.randn(1,3,224,224)  # [N C H W] for the Conv2D
+    model = VisionTransformer(config)
+    print(model(img))
